e5835c5bcf2c547d8fc406c76749e7a543c81ae0
Luis Santos <Luis.Santos@ingenico.com>
2020-04-26 15:59:13 +0200

    Better locking when deciding if a chain can proceed or not

--- src/main/java/com/ingenico/bamboo/bpgl/impl/GlobalPlanLockPreChainAction.java
+++ src/main/java/com/ingenico/bamboo/bpgl/impl/GlobalPlanLockPreChainAction.java
@@ -4,6 +4,8 @@ import static com.ingenico.bamboo.bpgl.impl.LockingConfiguration.LOCK_AQUIRE_RET
 
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +26,8 @@ public class GlobalPlanLockPreChainAction implements PreChainAction {
 
 	public static final Logger log = LoggerFactory.getLogger(GlobalPlanLockPreChainAction.class);
 
+	private static final Lock lock = new ReentrantLock(true);
+
 	@Override
 	public void execute(Chain chain, ChainExecution chainExecution) throws Exception {
 
@@ -34,38 +38,46 @@ public class GlobalPlanLockPreChainAction implements PreChainAction {
 			return;
 		}
 
+		PlanKey currentPlanKey = chain.getPlanKey();
 		String currentPlanLockKey = currentPlanLockKeyMaybe.get();
 
-		Optional<PlanKey> planKeyBeingExecuted = RunningPlans.getRunningPlanByLockKey(currentPlanLockKey);
+		try {
+			lock.lock();
+			Optional<PlanKey> planKeyBeingExecuted = RunningPlans.getRunningPlanByLockKey(currentPlanLockKey);
 
-		PlanKey currentPlanKey = chain.getPlanKey();
+			if (planKeyBeingExecuted.isPresent()) {
+				log.info("Build has a the global lock key {}. Only one build with that key can be executed at the same time", currentPlanLockKey);
 
-		if (planKeyBeingExecuted.isPresent()) {
-			chain.setSuspendedFromBuilding(true);
-			log.info(
-					"Build has a the global lock key {}. Only one build with that key can be executed at the same time",
-					currentPlanLockKey);
-
-			planKeyBeingExecuted = RunningPlans.getRunningPlanByLockKey(currentPlanLockKey);
-			while (planKeyBeingExecuted.isPresent()) {
-				try {
-					doWaitForPlan(planKeyBeingExecuted.get(), chainExecution);
-					planKeyBeingExecuted = RunningPlans.getRunningPlanByLockKey(currentPlanLockKey);
-				} catch (InterruptedException e) {
-					log.warn("Lock waiting interruped! - {}", e.getMessage());
-					PlanResultKey planResultKey = chainExecution.getPlanResultKey();
-					getChainExecutionManager().stop(planResultKey);
-					return;
+				planKeyBeingExecuted = RunningPlans.getRunningPlanByLockKey(currentPlanLockKey);
+				while (planKeyBeingExecuted.isPresent()) {
+					try {
+						// no not hog the lock when waiting
+						lock.unlock();
+						
+						doWaitForPlan(planKeyBeingExecuted.get());
+						
+						// but when we are done waiting take the lock for ourselves
+						lock.lock();
+						
+						// keep checking if we can proceed
+						planKeyBeingExecuted = RunningPlans.getRunningPlanByLockKey(currentPlanLockKey);
+					} catch (InterruptedException e) {
+						// It will reach here when, for example, someone cancels the build from the UI
+						log.warn("Lock waiting interruped! - {}", e.getMessage());
+						PlanResultKey planResultKey = chainExecution.getPlanResultKey();
+						getChainExecutionManager().stop(planResultKey);
+						return;
+					}
 				}
 			}
-			chain.setSuspendedFromBuilding(false);
-		}
 
-		RunningPlans.setRunningPlan(currentPlanLockKey, currentPlanKey);
+			RunningPlans.setRunningPlan(currentPlanLockKey, currentPlanKey);
+		} finally {
+			lock.unlock();
+		}
 	}
 
-	private void doWaitForPlan(PlanKey planKeyBeingExecuted, ChainExecution chainExecution)
-			throws InterruptedException {
+	private void doWaitForPlan(PlanKey planKeyBeingExecuted) throws InterruptedException {
 		// wait for ~1% of the build time or the default minimum
 		long minimumTimeToWait = TimeUnit.SECONDS.toMillis(LOCK_AQUIRE_RETRY_DELAY_SECONDS);
 
@@ -77,7 +89,6 @@ public class GlobalPlanLockPreChainAction implements PreChainAction {
 				planKeyBeingExecuted.toString(), TimeUnit.MILLISECONDS.toSeconds(timeToWaitMs));
 
 		Thread.sleep(timeToWaitMs);
-
 	}
 
 	public CachedPlanManager getCachedPlanManager() {
